aadbb7eb6fc45958f44f09aa02907ebc4d943bac
AesthetiCoder <luismonsalve1941@gmail.com>
2023-06-30 10:34:39 -0400

    :bug: fix: bug in a11y rules

--- src/module/a11y/rule/a11y_disabled_rule.ts
+++ src/module/a11y/rule/a11y_disabled_rule.ts
@@ -1,7 +1,8 @@
+/* eslint-disable unicorn/no-null */
 const a11yDisabledRule =
 {
-  'a11y/media-prefers-color-scheme': false,
-  'a11y/media-prefers-reduced-motion': false
+  'a11y/media-prefers-color-scheme': null,
+  'a11y/media-prefers-reduced-motion': null
 };
 
 export { a11yDisabledRule };
