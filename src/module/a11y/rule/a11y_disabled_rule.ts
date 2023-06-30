a27e222494e080a35393aa16ff3f75859aed22f1
AesthetiCoder <luismonsalve1941@gmail.com>
2023-06-30 10:37:17 -0400

    :sparkles: feat: change rules of a11y

--- src/module/a11y/rule/a11y_disabled_rule.ts
+++ src/module/a11y/rule/a11y_disabled_rule.ts
@@ -2,7 +2,8 @@
 const a11yDisabledRule =
 {
   'a11y/media-prefers-color-scheme': null,
-  'a11y/media-prefers-reduced-motion': null
+  'a11y/media-prefers-reduced-motion': null,
+  'a11y/line-height-is-vertical-rhythmed': null
 };
 
 export { a11yDisabledRule };
--- src/module/a11y/rule/a11y_general_rule.ts
+++ src/module/a11y/rule/a11y_general_rule.ts
@@ -8,8 +8,7 @@ const a11yGeneralRule =
   'a11y/font-size-is-readable': true,
   'a11y/no-obsolete-attribute': true,
   'a11y/selector-pseudo-class-focus': true,
-  'a11y/content-property-no-static-value': true,
-  'a11y/line-height-is-vertical-rhythmed': true
+  'a11y/content-property-no-static-value': true
 };
 
 export { a11yGeneralRule };
