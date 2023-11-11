8ff32a784ee4a354a1185a5a40d65ac355945f34
AesthetiCoder <luismonsalve1941@gmail.com>
2023-11-10 21:41:57 -0400

    :sparkles: feat: sass decreaped rules

--- src/module/sass/rule/sass_disabled_rule.ts
+++ src/module/sass/rule/sass_disabled_rule.ts
@@ -2,10 +2,12 @@
 const sassDisabledRule =
 {
   'at-rule-no-unknown': null,
+  'scss/at-import-no-partial-leading-underscore': null,
   'scss/at-mixin-named-arguments': null,
   'scss/at-rule-conditional-no-parentheses': null,
   'scss/dimension-no-non-numeric-values': null,
   'scss/map-keys-quotes': null,
+  'scss/no-duplicate-dollar-variables': null,
   'scss/operator-no-newline-after': null,
   'scss/partial-no-import': null
 };
