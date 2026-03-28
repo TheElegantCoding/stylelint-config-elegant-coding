b91e737c52da36d4f9376cb5c5d29c104555fed7
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-28 19:57:32 -0400

    feat: improve formatting in sass.ts for consistency

--- src/sass.ts
+++ src/sass.ts
@@ -1,12 +1,10 @@
 import { sassDisabledRule } from '@module/sass/rule/sass_disabled_rule';
 import { sassGeneralRule } from '@module/sass/rule/sass_general_rule';
 
-const sass =
-{
+const sass = {
   customSyntax: 'postcss-scss',
-  plugins: [ 'stylelint-scss' ],
-  rules:
-  {
+  plugins: ['stylelint-scss'],
+  rules: {
     ...sassDisabledRule,
     ...sassGeneralRule
   }
