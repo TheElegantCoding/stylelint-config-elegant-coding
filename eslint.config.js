0e49ac26f2a22d8805750dd7459d50039aad8843
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-28 19:56:32 -0400

    ✨ feat: update ESLint configuration to use universal code style

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-import elegantCoding from 'eslint-config-elegant-coding';
+import eslintConfig from 'eslint-config-universal-code';
 
-export default elegantCoding({
+const config = eslintConfig({
   json: true,
   stylistic: true,
   typescript: true,
+  perfectionist: true,
+  unicorn: true,
   yml: true
 });
+
+export default config;
