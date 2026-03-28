f3cfeadc0797ff97e3c643763d796a10d4c7a17f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-28 19:57:35 -0400

    feat: improve formatting in index.ts for consistency

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,9 @@ import { disabledRule } from '@module/stylelint/rule/disabled_rule';
 import { generalRule } from '@module/stylelint/rule/general_rule';
 import { stylistic } from '@module/stylistic/stylistic';
 
-const base =
-{
-  plugins: [ ...order.plugins, ...stylistic.plugins ],
-  rules:
-  {
+const base = {
+  plugins: [...order.plugins, ...stylistic.plugins],
+  rules: {
     ...disabledRule,
     ...generalRule,
     ...order.rules,
