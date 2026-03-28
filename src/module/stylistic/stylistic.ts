807e7f0b01009c0e37cc415e7defa8ef4c090e56
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-28 19:56:49 -0400

    ✨ feat: remove unnecessary whitespace in plugins array in stylistic.ts

--- src/module/stylistic/stylistic.ts
+++ src/module/stylistic/stylistic.ts
@@ -2,7 +2,7 @@ import { stylisticDisabledRule } from '@module/stylistic/rule/stylistic_disabled
 import { stylisticGeneralRule } from '@module/stylistic/rule/stylistic_general_rule';
 
 const stylistic = {
-  plugins: [ '@stylistic/stylelint-plugin' ],
+  plugins: ['@stylistic/stylelint-plugin'],
   rules: {
     ...stylisticGeneralRule,
     ...stylisticDisabledRule
