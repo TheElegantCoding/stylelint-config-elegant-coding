1a775f868099308578fea21757b5305895cb9930
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-01 21:01:57 -0400

    feat: update tsup configuration for improved build settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [ 'src/index.ts', 'src/sass.ts' ],
-  shims: true
+  entry: ['src/index.ts', 'src/sass.ts'],
+  format: ['esm'],
+  clean: true,
+  dts: true,
+  minify: true
 });
